e5149e5006d8d4c63f76595c977f5aa6767565d0
Joseph Spiros <joseph.spiros@ithinksw.com>
2011-04-13 03:27:50 -0400

    Fixed handling of null-termination in the string and unicode string readers.

--- ebml/core.py
+++ ebml/core.py
@@ -189,7 +189,7 @@ def read_string(stream, size):
 	value = ''
 	if size > 0:
 		value = stream.read(size)
-		value = value.rstrip(chr(0))
+		value = value.partition(chr(0))[0]
 	return value
 
 
@@ -209,7 +209,7 @@ def read_unicode_string(stream, size):
 	value = u''
 	if size > 0:
 		data = stream.read(size)
-		data = data.rstrip(chr(0))
+		data = data.partition(chr(0))[0]
 		value = unicode(data, 'utf_8')
 	return value
 
